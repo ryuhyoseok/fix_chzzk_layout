d5227e989f30a0efb3167efdb72eb37e19e50ff0
ryuhyoseok <ohmyhaker@gmail.com>
2023-12-22 21:51:13 +0900

    Remove console.log

--- optionHandler.js
+++ optionHandler.js
@@ -13,8 +13,6 @@
     const responsiveWidthInput = document.querySelector('#responsiveWidthInput')
     const responsiveRatioInput = document.querySelector('#responsiveRatioInput')
 
-    console.log('responsiveRatioInput', responsiveRatioInput)
-
     const {layoutType, responsiveWidth = 900, responsiveRatio = '5/6' } = (await chrome.storage.sync.get(['layoutType', 'responsiveWidth', 'responsiveRatio']) || {})
 
     if (layoutType) {
@@ -27,7 +25,6 @@
     }
 
     if (responsiveRatio) {
-        console.log('123', responsiveRatio)
         responsiveRatioInput.value = responsiveRatio
     }
 
@@ -36,18 +33,15 @@
     radios.forEach((radio) => {
         radio.addEventListener('change', () => {
             if (!radio.checked) return
-            console.log('responsiveWidthInput', responsiveWidthInput.value)
             chrome.storage.sync.set({ layoutType: radio.id, responsiveWidth: responsiveWidthInput.value, responsiveRatio: (responsiveRatioInput.value || '').trim() })
         })
     })
 
     responsiveWidthInput.addEventListener('input', debounce(() => {
-        console.log('set', responsiveWidthInput.value)
         chrome.storage.sync.set({ responsiveWidth: responsiveWidthInput.value })
     }, 500))
 
     responsiveRatioInput.addEventListener('input', debounce(() => {
-        console.log('set', responsiveWidthInput.value)
         chrome.storage.sync.set({ responsiveRatio: (responsiveRatioInput.value || '').trim() })
     }, 500))
 })()
